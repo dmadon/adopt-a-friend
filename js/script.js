dad32702c3910f37af1970bec5b488a55d0dee25
dmadon <deanna.madon@gmail.com>
2022-07-08 16:38:14 -0500

    got new access token

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ var breedInputEl = document.querySelector("#breed");
 
 var APIkey = "iRFrneB2mkJOOzVLPMTPpShKXk7easKfumf7IM75Xnwba8w8tq";
 var secret = "sEVE2RVHpkQuVbJkDVdwWZQDMCoyNgYk5EGdVvqP";
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJpUkZybmVCMm1rSk9PelZMUE1UUHBTaEtYazdlYXNLZnVtZjdJTTc1WG53YmE4dzh0cSIsImp0aSI6ImJkMzkxYmM1MDY2ZmY1ZTRlNDg1NjUwMDA0MTdhN2RjYzU5Njk0ZGI3YjMwZjRhYmJiOGM0MDZiNDkyYTViODU0YTFkYzMyZDVmOTNkODU4IiwiaWF0IjoxNjU3MzA5Nzk2LCJuYmYiOjE2NTczMDk3OTYsImV4cCI6MTY1NzMxMzM5Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.df-iGSLnSlwX0m-f-renbdQ71gXLw82nkbfI6WTqbXgBPVmfAsZ92r6WvcB2Dt71TD41DoeboqkZvlTnDLLGTOz2NC3zbQ7knsdvwBXELwAB8FXvG6V89accb78lzFC3RV44yii6GdcWouhVX2TDSThIVVV_opJxwOrndveW68kD5LWuuN-KHrfBsViCLh3y32ogfReKZGVs-J3b9fHqPaQQ2htEss4pEUVJsrsAe1k2PD6yFZu2Z_bVe4-sVd44hwhZ_hlzZFNSjzboFXhsoDdTsgawUJSunz2ch2pswK7ek96oo-GAMZrJytLPyyaykNZ82OhTRnxyxPQ7SBf9cQ";
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJpUkZybmVCMm1rSk9PelZMUE1UUHBTaEtYazdlYXNLZnVtZjdJTTc1WG53YmE4dzh0cSIsImp0aSI6IjE2ZjdhZWIwNzcwMDFmNjY3ZTYxYTI0NDgzYmNlMDg3NjZmNTNhN2MzMmMxNTJiMWY4OTNjMjc5MzFiYjFhMjhkZmMxNTg1MjQ0MTA5OTgzIiwiaWF0IjoxNjU3MzEzNTUwLCJuYmYiOjE2NTczMTM1NTAsImV4cCI6MTY1NzMxNzE1MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.oX2YCJgqndppTNcC1XL6h_TApnRbCh3XcM8n3poyWEalFAazhO4f64E36_jbTk0AAdTd7F4VjMN_4nBSvEqctJAyf_hhyYj53FF1Q3Ilc7BCJosi0xj8Q5HsRHhl5U-x4GZAGMmsDuJucDodn0wCf7LgPLaxvTi0bZMYHxJzDaMZZ6eVVcZYR4f2ZN9Oj-eoVerQSufFjVShI6GD_jsl4_E0nmNM6hkvBD4_1RohcqDkqwuDbIhHaTEn-Ji9xgifeGGz7jvd-dW_4d0SUVU9bVDu9xU3EvW8a1Y9-vwZ1CLAxvGkSnlphSsic0Zl6NOAnDln44Uw5im1200WycnTnw";
 
 // get new access token by requesting from the command line (copy and paste this into the command line prompt):
 // curl -d "grant_type=client_credentials&client_id=iRFrneB2mkJOOzVLPMTPpShKXk7easKfumf7IM75Xnwba8w8tq &client_secret=sEVE2RVHpkQuVbJkDVdwWZQDMCoyNgYk5EGdVvqP" https://api.petfinder.com/v2/oauth2/token
-// once you have been granted a new access token, set the "token" variable above to the new token
+// once you have been granted a new access token, replace the "token" variable above with the new token
 
 
 var getInfo = function(){
@@ -35,9 +35,18 @@ fetch(queryURL,{headers:{"Authorization":"Bearer "+token}})
                     // CREATE ANIMAL CARD
                     var animalCard = document.createElement("div");
                     animalCard.id=(data.animals[i].id);
-                    animalCard.classList=("card horizontal col s12 m6 l4 animal-card ");
+                    animalCard.classList=("card horizontal col s12 m6 l4 animal-card border");
                         
                     // INSERT ANIMAL PHOTO ON LEFT SIDE OF CARD
+
+                    // container that holds the photo, set to a fixed width and height
+                    var photoContainer = document.createElement("div");
+                    photoContainer.classList=("card-image justify-content-center border-right");
+                    photoContainer.id=("photoContainer");
+
+
+
+                    // animal photo to append to the photo container
                     var primPhoto = document.createElement("img");
                     primPhoto.classList=("card-image animal-info-item");
                         if(data.animals[i].primary_photo_cropped){
@@ -47,8 +56,10 @@ fetch(queryURL,{headers:{"Authorization":"Bearer "+token}})
                             var thumbnail = "./images/paw-heart-gray.png";
                         }
                     primPhoto.src=(thumbnail);
-                    primPhoto.height=(240);
-                    animalCard.appendChild(primPhoto);                    
+                    primPhoto.height=(180);
+                    photoContainer.appendChild(primPhoto);    
+                    
+                    animalCard.appendChild(photoContainer);
 
 
                     // INSERT ANIMAL INFO ON RIGHT SIDE OF CARD
@@ -59,7 +70,7 @@ fetch(queryURL,{headers:{"Authorization":"Bearer "+token}})
                      
                         // first div on right side of card: this is the green header on the animal card that contains the animal's name
                         var nameWrapper = document.createElement("div");
-                        nameWrapper.classList=("card-title green darken-2 white-text center-align");
+                        nameWrapper.classList=("card-title green darken-2 white-text center-align border-bottom");
 
                             // animal name that appears inside the green header
                             var petName = document.createElement("p");
