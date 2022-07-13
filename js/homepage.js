79a58692545be0fe34ac550720e0d48061315f5c
Padilla2012 <padilla-silva@hotmail.com>
2022-07-12 21:37:29 -0500

    added drop down

--- js/homepage.js
+++ js/homepage.js
@@ -223,13 +223,11 @@ var markFavs = function(){
         }
     }
 }
-
-
-
-
-animalInfoEl.addEventListener("click", saveFavorite);
-
-document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
+    var instance = M.Dropdown.getInstance(elem, options);
+    instance.open();
+    instance.close();
   });
+
