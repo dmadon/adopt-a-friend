a60238a89b70827e00bc2c3235296383f375203c
Padilla2012 <padilla-silva@hotmail.com>
2022-07-12 20:23:15 -0500

    laber data input added modifications

--- js/homepage.js
+++ js/homepage.js
@@ -228,3 +228,8 @@ var markFavs = function(){
 
 
 animalInfoEl.addEventListener("click", saveFavorite);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
